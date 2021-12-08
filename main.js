0e38e4c8ab8ecee14e79a16740bfe44e48f2abfb
khalil kraiem <khalilmedkraiem@gmail>
2021-12-08 15:33:08 +0100

    start my project

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    
+});
+//////////Principal Div///////////
+function DivAdmin(){
+    
+}
+function DivClient(){
+
+}
+//////////Secondary Div///////////
+function creatDivClient(){
+
+}
+function creatDivAdmin(){
+    divAdmin.append()
+    var name=$('#nameAdmin').val()
+    var mail=$('#mailAdmin').val() 
+
+    makeAdmin(name,mail,telephne,password,username)
+}
+function createDivProduct(){
+
+}
+function creatDivJournal(){
+
+}
+function creatDivFacturation(){
+
+}
+//////////Data Modeling///////////
+function makeClient(){
+
+}
+
